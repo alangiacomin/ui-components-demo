1fde66d2d9d8583a97fbeff00e3a05de0db9c5a3
Alan Giacomin <alan.giacomin@gmail.com>
2021-04-04 20:57:45 +0200

    translation pagina due

--- resources/js/pages/PaginaDue/PaginaDue.jsx
+++ resources/js/pages/PaginaDue/PaginaDue.jsx
@@ -3,31 +3,35 @@ import Col from '../../components/Col/Col';
 import ColBreak from '../../components/ColBreak/ColBreak';
 import LayoutMain from '../../components/LayoutMain/LayoutMain';
 import Row from '../../components/Row/Row';
+import useTranslation from '../../hooks/useTranslation';
 
-const PaginaDue = () => (
-  <LayoutMain>
-    <h1>PAGINA DUE</h1>
-    <Row>
-      <Col>Colonna 6</Col>
-      <Col>Colonna 6</Col>
-      <ColBreak />
-      <Col className="col-8">Colonna 8</Col>
-      <Col className="col-4">Colonna 4</Col>
-      <ColBreak />
-      <Col className="col">Colonna 2</Col>
-      <Col className="col">Colonna 2</Col>
-      <Col className="col">Colonna 2</Col>
-      <Col className="col">Colonna 2</Col>
-      <Col className="col">Colonna 2</Col>
-      <Col className="col">Colonna 2</Col>
-    </Row>
-    <Row>
-      <Col>
-        <p><Link to="/">Pagina uno</Link></p>
-        <p><Link to="/paginaDue">Pagina due</Link></p>
-      </Col>
-    </Row>
-  </LayoutMain>
-);
+const PaginaDue = () => {
+  const { t } = useTranslation('pageTwo');
+  return (
+    <LayoutMain>
+      <h1>PAGINA DUE</h1>
+      <Row>
+        <Col>{t('colonna')} 6</Col>
+        <Col>{t('colonna')} 6</Col>
+        <ColBreak />
+        <Col className="col-8">{t('colonna')} 8</Col>
+        <Col className="col-4">{t('colonna')} 4</Col>
+        <ColBreak />
+        <Col className="col">{t('colonna')} 2</Col>
+        <Col className="col">{t('colonna')} 2</Col>
+        <Col className="col">{t('colonna')} 2</Col>
+        <Col className="col">{t('colonna')} 2</Col>
+        <Col className="col">{t('colonna')} 2</Col>
+        <Col className="col">{t('colonna')} 2</Col>
+      </Row>
+      <Row>
+        <Col>
+          <p><Link to="/">Pagina uno</Link></p>
+          <p><Link to="/paginaDue">Pagina due</Link></p>
+        </Col>
+      </Row>
+    </LayoutMain>
+  );
+};
 
 export default PaginaDue;
---
+++ resources/lang/en/pageTwo.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'colonna' => 'Column',
+];
